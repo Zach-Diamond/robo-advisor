18be3ec741d16418946d59d019bcea58db012a04
Zach-Diamond <50925443+Zach-Diamond@users.noreply.github.com>
2019-06-19 20:46:37 -0400

    Mostly Done - Working on Export

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -12,7 +12,7 @@ import numpy as np
 load_dotenv()
 
 
-###FUNCTIONS
+########FUNCTIONS
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)  
     #https://stackoverflow.com/questions/19859282/check-if-a-string-contains-a-number
@@ -24,15 +24,14 @@ def to_usd(my_price):
      return "${0:,.2f}".format(float(my_price))
 
 
-
-###LATER-USED VARIABLES
+########SCRIPT-WIDE VARIABLES
 apikey = os.environ.get("ALPHAVANTAGE_API_KEY")
 todaydate = datetime.now().strftime('%Y-%m-%d')
 yesterday = date.today() - timedelta(days=1)
 yesterdaydate = yesterday.strftime('%Y-%m-%d')
 
 
-###USER INPUT 
+########USER INPUT 
 entered_stock = input('Please type a stock symbol and press ENTER: ').lower()
 
 while (len(entered_stock) > 5 or len(entered_stock) < 1 or hasNumbers(entered_stock)==True):
@@ -40,22 +39,20 @@ while (len(entered_stock) > 5 or len(entered_stock) < 1 or hasNumbers(entered_st
 else: None
 
 
-###REQUEST API AND CREATE VARIABLES
+########REQUEST API AND CREATE VARIABLES
 request_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={entered_stock}&apikey={apikey}'
 response = requests.get(request_url)
-# if response:
-#     response
-# else:
-#     print('no worko')
-#     quit()
 parsed_response = json.loads(response.text)
+
+#PREVENTING ERRORS AT RESPONSE
 try:
     parsed_response['Time Series (Daily)']
 except:
     print("Stock symbol not recognized. Please restart and try again.")
     exit()
-parsed_timeseries = parsed_response['Time Series (Daily)']
 
+parsed_timeseries = parsed_response['Time Series (Daily)']
+#PREVENTING ERRORS AT RESPONSE
 try:
     parsed_response['Time Series (Daily)'][todaydate]
 except:
@@ -65,19 +62,19 @@ except:
 today_parsed_timeseries = parsed_response['Time Series (Daily)'][todaydate]
 
 
-###Creating TODAY Variables
+########VARIABLES TO BE USED
+#Creating TODAY Variables
 today_open = float(today_parsed_timeseries['1. open'])
 today_close = float(today_parsed_timeseries['4. close'])
 today_high = float(today_parsed_timeseries['2. high'])
 today_low = float(today_parsed_timeseries['3. low'])
 yesterday_close = float(parsed_response['Time Series (Daily)'][yesterdaydate]['4. close'])
 
-
-###Creating TOTAL Variables
+#Creating TOTAL Variables
 daily_highs = [float(parsed_timeseries[i]['2. high']) for i in parsed_timeseries]
 daily_lows = [float(parsed_timeseries[i]['3. low']) for i in parsed_timeseries]
 
-###Creating average/max/min
+#Creating average/max/min
 daily_high_average = np.average(daily_highs)
 daily_lows_average = np.average(daily_lows)
 daily_high_max = np.max(daily_highs)
@@ -86,68 +83,74 @@ daily_high_min = np.min(daily_highs)
 daily_lows_min = np.min(daily_lows)
 daily_high_std = np.std(daily_highs)
 daily_lows_std = np.std(daily_lows)
+
+#Attempting to calculate slope - help from https://stackoverflow.com/questions/53100393/how-to-get-slope-from-timeseries-data-in-pandas
 df = pd.DataFrame([(i,float(parsed_timeseries[i]['2. high'])) for i in parsed_timeseries], columns=['date','high'])
-df['date_ordinal'] = pd.to_datetime(df['date']).map(datetime.datetime.toordinal)
+df['high'] = pd.to_numeric(df['high'], errors='coerce')
+df['date']=pd.to_datetime(df['date'])
+df['date_ordinal'] = pd.to_datetime(df['date']).map(datetime.toordinal)
 reg = linear_model.LinearRegression()
 reg.fit(df['date_ordinal'].values.reshape(-1, 1), df['high'].values)
-
-reg.coef_
-
-array([0.80959405])
+slope = float(reg.coef_)
 
 
-
-###CREATING SUPER ADVANCED RECOMMENDATION ENGINE OF THE FUTURE
+########CREATING SUPER ADVANCED RECOMMENDATION ENGINE OF THE FUTURE
 total_score = 0
 reasons = []
 
+#Is the slope positive and close to 1?
+if slope > .5:
+    total_score+=4
+    reasons.append("(+) This stock has a positive and strong slope.")
+elif slope < .5 and slope > 0:
+    total_score+=2
+    reasons.append("(+) This stock has a positive slope.")
+else:
+    total_score+=-2
+    reasons.append("(-) This stock has a negative slope.")
+
 #Is today's low  > daily high average?
 if (today_low/daily_high_average)-1 > .2:
-    total_score+=3
-    reasons.append("Today's low is >20% vs. average daily high!")
+    total_score+=4
+    reasons.append("(++) Today's low is >20% vs. its average daily high!")
 elif (today_low/daily_high_average)-1 > .1:
     total_score+=2
-    reasons.append("Today's low is >10% vs. average daily high.")
+    reasons.append("(+) Today's low is >10% vs. its average daily high.")
 else:
     total_score+=0
-    reasons.append("Today's low is not very strong vs. daily high.")
+    reasons.append("(-) Today's low is not very strong vs. its average daily high.")
 
 #Is today's low more than 1.5 St.D. over average daily high?
 if today_low > daily_high_average+(daily_high_std*2):
-    total_score+=2
-    reasons.append("Today's low is more than 2 deviations over the average daily high!")
+    total_score+=3
+    reasons.append("(++) Today's low is more than 2 deviations over the average daily high!")
 if today_low > daily_high_average+(daily_high_std):
     total_score+=1
-    reasons.append("Today's low is more than 1 deviation over the average daily high!")
+    reasons.append("(+) Today's low is more than 1 deviation over the average daily high!")
 else:
     total_score+=0
-    reasons.append("Today's low is not remarkable vs. the average daily high's standard deviation.")
+    reasons.append("(-) Today's low is not remarkable vs. the average daily high's standard deviation.")
 
 #Is the opening price > yesterday's close?
 if today_open>yesterday_close:
     total_score+=1
-    reasons.append("Today's open is higher than yesterday's close!")
+    reasons.append("(+) Today's open is higher than yesterday's close!")
 else:
     total_score+=0
-    reasons.append("Today's open is below yesterday's close.")
+    reasons.append("(-) Today's open is below yesterday's close.")
 
 #Is the opening price > daily high average?
 if (today_open/daily_high_average)-1 > .2:
     total_score+=2
-    reasons.append("Today's open is >20% vs. average daily high!")
+    reasons.append("(+) Today's open is >20% vs. average daily high!")
 elif (today_open/daily_high_average)-1 > .1:
     total_score+=1
-    reasons.append("Today's open is >10% vs. average daily high.")
+    reasons.append("(+) Today's open is >10% vs. average daily high.")
 else:
     total_score+=0
-    reasons.append("Today's open is not very strong vs. daily high.")
+    reasons.append("(-) Today's open is not very strong vs. daily high.")
 
-
-
-#Max = 8
-
-print(total_score)
-print(reasons)
+reasons_clean = [i for i in reasons]
 
 ###PRINT RESULTS
 print('--------------------------------------')
@@ -163,24 +166,46 @@ print(f"CLOSING PRICE: {to_usd(today_close)}")
 print(f"DAILY HIGH: {to_usd(today_high)}")
 print(f"DAILY LOW: {to_usd(today_low)}")
 print(f"TOTAL VOLUME: {add_commas(int(today_parsed_timeseries['5. volume']))}")
+print('--------------------------------------')
+if total_score >= 9:
+    print("RECOMMENDATION: STRONG BUY!")
+elif total_score >= 6:
+    print("RECOMMENDATION: BUY")
+elif total_score >=4:
+    print("RECOMMENDATION: CONSIDER")
+elif total_score <3 and total_score >0:
+    print("RECOMMENDATION: AVOID")
+elif total_score <= 0:
+    print("RECOMMENDATION: STRONG AVOID!")
+else:
+    print("RECOMMENDATION: STRONG AVOID!")
+print(f'RECOMMENDATION DETAILS: ')
 
+for c, value in enumerate(reasons_clean, 1): # https://www.geeksforgeeks.org/enumerate-in-python/
+    print(c, value)
+print(f"Total score: {total_score} of possible 14 points.")
 
+#Export to file based on stock name (and append if already exists)
+with open(f'../data/prices_{entered_stock}.csv', 'a') as csvFile:
+    fieldnames = ['1. open', '2. high', '3. low', '4. close', '5. volume']
+    writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow (today_parsed_timeseries)
+csvFile.close()
 
-
-
-
-
-
-
-    #  T E M P O R A R I L Y   D I S A B L E D 
-# #Export to file based on stock name (and append if already exists)
+# keys = parsed_timeseries[0].keys()
 # with open(f'../data/prices_{entered_stock}.csv', 'a') as csvFile:
-#     fieldnames = ['1. open', '2. high', '3. low', '4. close', '5. volume']
-#     writer = csv.DictWriter(csvFile, fieldnames=fieldnames)
-#     writer.writeheader()
-#     writer.writerow (today_parsed_timeseries)
-# csvFile.close()
+#  # Using dictionary keys as fieldnames for the CSV file header
+#     dict_writer = csv.DictWriter(csvFile, keys)
+#     dict_writer.writeheader()
+#     dict_writer.writerows(parsed_timeseries)
 
+print('--------------------------------------')
+print('GOOD LUCK INVESTING!')
+print('I am not liable for any money lost')
+print('due to the questionable nature of')
+print('this "algorithm." That is all on you!')
+print('--------------------------------------')
 
 # print("RECOMMENDATION: BUY!")
 # print("RECOMMENDATION REASON: TODO")
